41d8fe915fe2f183b0be77982fda1918e63ab5f2
Moflop <8089341@qq.com>
2022-12-05 21:07:59 +0800

    Create branch.

---
+++ src/main/java/render/engine/RenderEngine.java
@@ -0,0 +1,14 @@
+package render.engine;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author Arcomit
+ * @updateDate 2022/11/13
+ */
+@Mod(RenderEngine.MODID)
+public class RenderEngine
+{
+    public static final String MODID= "renderengine";
+
+}
